27f2ddf3e8bf169db237da09b9334e1cbe89c82c
AzukiPL <102173527+AzukiPL@users.noreply.github.com>
2023-10-17 22:59:12 +0200

    back to main

--- scripts/Chess/PawnsParameters.js
+++ scripts/Chess/PawnsParameters.js
@@ -12,4 +12,26 @@ class PawnsParameters {
         this.rangeCol = [];
         this.object = document.getElementById(this.tileID);
     }
+
+    onChessSelect() {
+        this.object = document.getElementById(this.tileID+" Pawn");
+        this.object.addEventListener("click", (e) => {
+            this.refreshTiles();
+            // document.getElementById(String(this.row)+String(this.column)).classList.add("game-Chess-selected");
+            for(let i=0; i<this.rangeRow.length;i++) {
+                if(document.getElementById(String(parseInt(this.row+this.rangeRow[i]))+String(parseInt(this.column+this.rangeCol[i]))) != null)
+                {
+                    const tile = document.getElementById(String(parseInt(this.row+this.rangeRow[i]))+String(parseInt(this.column+this.rangeCol[i])));
+                    tile.classList.add("game-Chess-selected");
+                }
+            }
+        }, false);
+    } 
+    refreshTiles() {
+        const tiles = document.getElementsByClassName("game-Chess-selected");
+        for(let i=tiles.length-1; 0<=i; i--) {
+            tiles[i].classList.remove("game-Chess-selected");
+        }
+    }
+
 }
---
+++ scripts/Chess/TileListener.js
@@ -0,0 +1,42 @@
+class TileListener {
+    constructor () {
+        this.lastSelected = null;
+    }
+    addListeners() {
+        const tiles = document.getElementsByClassName("game-Chess-Tile");
+        for (const iterator of tiles) {
+            iterator.addEventListener("click", (e) => {
+                if(e.target.classList.contains("game-Chess-pawn-images") && this.lastSelected == null) {
+                    this.lastSelected = e.target;
+                }
+                else if(e.target.classList.contains("game-Chess-pawn-images")) {
+                    if(this.lastSelected.customObject.color != e.target.customObject.color) {
+                        if(iterator.classList.contains("game-Chess-selected")) {
+                            e.stopPropagation();
+                            iterator.innerHTML = " ";
+                            this.#chessMove(iterator);
+                        }
+                        else this.lastSelected = e.target;
+                    }
+                    else this.lastSelected = e.target;
+                }
+                else this.#chessMove(iterator);
+            }, true);
+        }
+    }
+    #refreshTiles() {
+        const tiles = document.getElementsByClassName("game-Chess-selected");
+        for(let i=tiles.length-1; 0<=i; i--) {
+            tiles[i].classList.remove("game-Chess-selected");
+        }
+    }
+
+    #chessMove(iterator = document) {
+        if(iterator.classList.contains("game-Chess-selected")) {
+            iterator.appendChild(this.lastSelected);
+            this.#refreshTiles();
+            this.lastSelected.customObject.row = parseInt(iterator.id / 10);
+            this.lastSelected.customObject.column = parseInt(iterator.id % 10);
+        }
+    }
+}
